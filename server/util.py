4cd9d857d1e62add28927f75b75acaf15bf30769
Victoria Slocum <victorialslocum@gmail.com>
2023-02-09 14:44:45 +0100

    fix tests

--- server/util.py
+++ server/util.py
@@ -51,36 +51,36 @@ def format_docstrings(docstring: str):
         # if the docstring doesn't include multiple ":" in the last paragraph
         # then it doesn't have an arguments list, just return the docstring without formatting
         return docstring
-    else:
-        # some docstrings have different formatting than others
-        # differentiated by the amount of spaces after a \n
-        if "\n       " in docstring:
-            # create the arguments list from the last paragraph,
-            # remove unnessesary \n and replace others with paragraph breaking \n\n and bullet points
-            registry_arguments = (
-                docstring.split("\n\n")[-1][:-2]
-                .replace("\n       ", "")
-                .replace("\n   ", "\n\n - ")
-            )
-            # reformat all other paragraphs
-            # remove unnessesary \n and replace others with paragraph breaking \n\n
-            registry_info = (
-                "\n\n".join(docstring.split("\n\n")[:-2])
-                .replace("\n   ", "")
-                .replace("\n", "\n\n")
-            )
-        else:
-            # create the arguments list from the last paragraph,
-            # remove unnessesary \n and replace others with paragraph breaking \n\n and bullet points
-            registry_arguments = (
-                docstring.split("\n\n")[-1]
-                .replace("\n   ", "")
-                .replace("\n", "\n\n - ")
-            )
-            # remove last paragraph from docstring
-            registry_info = "\n\n".join(docstring.split("\n\n")[:-1])
 
-        formatted_docstring = (
-            f"{registry_info}\n#### Arguments:\n\n - {registry_arguments}"
+    # some docstrings have different formatting than others
+    # differentiated by the amount of spaces after a \n
+    if "\n       " in docstring:
+        # create the arguments list from the last paragraph,
+        # remove unnessesary \n and replace others with paragraph breaking \n\n and bullet points
+        registry_arguments = (
+            docstring.split("\n\n")[-1][:-2]
+            .replace("\n       ", "")
+            .replace("\n   ", "\n\n - ")
         )
-        return formatted_docstring
+        # reformat all other paragraphs
+        # remove unnessesary \n and replace others with paragraph breaking \n\n
+        registry_info = (
+            "\n\n".join(docstring.split("\n\n")[:-2])
+            .replace("\n   ", "")
+            .replace("\n", "\n\n")
+        )
+    else:
+        # create the arguments list from the last paragraph,
+        # remove unnessesary \n and replace others with paragraph breaking \n\n and bullet points
+        registry_arguments = (
+            docstring.split("\n\n")[-1]
+            .replace("\n   ", "")
+            .replace("\n", "\n\n - ")
+        )
+        # remove last paragraph from docstring
+        registry_info = "\n\n".join(docstring.split("\n\n")[:-1])
+
+    formatted_docstring = (
+        f"{registry_info}\n#### Arguments:\n\n - {registry_arguments}"
+    )
+    return formatted_docstring
