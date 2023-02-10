fb2ffd4de16a3fb1c1d590ed1c2d50f289ec4370
Victoria <80417010+victorialslocum@users.noreply.github.com>
2023-02-10 13:24:08 +0100

    Finalize hover feature (#9)
    * finalize hover feature
    
    * change registry code link to relative
    
    * Update server/feature_hover.py
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>
    
    * documentation, section_resolver variable refactor
    
    * move docstring format to util.py
    
    * test, refactor, comments for docstring format func
    
    * fix tests
    
    * fix formatting
    
    ---------
    
    Co-authored-by: Madeesh Kannan <shadeMe@users.noreply.github.com>

--- server/feature_hover.py
+++ server/feature_hover.py
@@ -14,7 +14,7 @@ import re
 from typing import Optional
 from spacy import registry, schemas, glossary
 from .spacy_server import SpacyLanguageServer
-from .util import get_current_word, SpanInfo
+from .util import get_current_word, SpanInfo, format_docstrings
 from dataclasses import dataclass
 from catalogue import RegistryError
 
@@ -22,20 +22,13 @@ from catalogue import RegistryError
 CONFIG_DESCRIPTIONS = {
     "nlp": "Definition of the `Language` object, its tokenizer and processing pipeline component names.",
     "components": "Definitions of the pipeline components and their models. Pipeline components can be found in [nlp].",
-    "paths": "Paths to data and other assets. Re-used across the config as variables, e.g. ${paths.train}, and can be overridden by the CLI.",
-    "system": "Settings related to system and hardware. Re-used across the config as variables, e.g. ${system.seed}, and can be overridden by the CLI.",
+    "paths": "Paths to data and other assets. Re-used across the config as variables, e.g. `${paths.train}`, and can be overridden by the CLI.",
+    "system": "Settings related to system and hardware. Re-used across the config as variables, e.g. `${system.seed}`, and can be overridden by the CLI.",
     "training": "Settings and controls for the training and evaluation process.",
     "pretraining": "Optional settings and controls for the language model pretraining.",
     "initialize": "Data resources and arguments passed to components when `nlp.initialize` is called before training (but not at inference-time).",
     "corpora": "Readers for corpora like dev and train.",
 }
-
-
-@dataclass
-class HoverInfo:
-    display_string: str  # Can have markdown elements
-    start: int  # Start index of the source string
-    end: int  # End index of the source string
 
 
 def hover(
@@ -49,6 +42,9 @@ def hover(
     if not document:
         return None
 
+    # get the config as a dictinary
+    config_dict = server.config
+
     line_n = params.position.line
     line_str = document.lines[line_n]
     current_span = get_current_word(line_str, params.position.character)
@@ -60,11 +56,13 @@ def hover(
         hover_object = section_resolver(
             line_str, current_span.span_string, current_span.start, current_span.end
         )
+    if hover_object is None:
+        hover_object = variable_resolver(line_str, config_dict)
 
     if hover_object is not None:
         return Hover(
             contents=MarkupContent(
-                kind=MarkupKind.Markdown, value=hover_object.display_string
+                kind=MarkupKind.Markdown, value=hover_object.span_string
             ),
             range=Range(
                 start=Position(line=line_n, character=hover_object.start),
@@ -77,7 +75,7 @@ def hover(
 
 def registry_resolver(
     line_str: str, current_word: str, w_start: int, w_end: int
-) -> Optional[HoverInfo]:
+) -> Optional[SpanInfo]:
     """
     Check if currently hovered text is registered in the spaCy registry and return its description.
 
@@ -90,13 +88,16 @@ def registry_resolver(
 
     registry_span = detect_registry_func(line_str, current_word, w_start, w_end)
     if registry_span:
+        # the name of the function within the registry
         registry_func = registry_span.span_string
         r_start = registry_span.start
         r_end = registry_span.end
+        # the name of the registry, e.x. "architectures" or "factory"
         registry_name = detect_registry_name(line_str, r_start)
     else:
         return None
 
+    # hardcoded renaming for factories
     if registry_name == "factory":
         registry_name = "factories"
 
@@ -107,25 +108,32 @@ def registry_resolver(
     except RegistryError as e:
         return None
 
+    # get the path to the file and line number of registered function
+    registry_link = ""
+    if registry_desc["file"]:
+        registry_path = registry_desc["file"]
+        line_no = registry_desc["line_no"]
+        registry_link = f"[Go to code]({registry_path}#L{line_no})"
+
+    # find registry description or return no description
     registry_docstring = (
         registry_desc.get("docstring") or "Currently no description available"
     )
 
-    # TODO Create link to codebase (if possible)
-    # if registry_desc["file"] is not None:
-    # registry_path = f"➡️ [Go to code]({registry_desc['file']})"
+    # Fix the formatting of docstrings for display in hover
+    formatted_docstring = format_docstrings(registry_docstring)
 
-    # TODO Fine-Tune display message
-    hover_display = f"## ⚙️ {registry_func}\n{registry_docstring}"
-
-    return HoverInfo(hover_display, r_start, r_end)
+    hover_display = (
+        f"### (*registry*) {registry_func}\n\n{registry_link}\n\n{formatted_docstring}"
+    )
+    return SpanInfo(hover_display, r_start, r_end)
 
 
 def detect_registry_func(
     line: str, current_word: str, w_start: int, w_end: int
 ) -> Optional[SpanInfo]:
     """
-    Detect if a word indicates a registry name
+    Detect if a word indicates a registry name.
 
     ARGUMENTS:
     line (str): the current line as a string.
@@ -164,7 +172,7 @@ def detect_registry_func(
 
 def detect_registry_name(line: str, registry_start: int) -> str:
     """
-    Detect type and return type and name of registry
+    Detect type and return type and name of registry.
 
     ARGUMENTS:
     line (str): the current line as a string.
@@ -175,6 +183,7 @@ def detect_registry_name(line: str, registry_start: int) -> str:
 
     EXAMPLE:
     @architecture
+    factory
     """
 
     if registry_start <= 0:
@@ -190,10 +199,9 @@ def detect_registry_name(line: str, registry_start: int) -> str:
 
 def section_resolver(
     line_str: str, current_word: str, w_start: int, w_end: int
-) -> Optional[HoverInfo]:
-
+) -> Optional[SpanInfo]:
     """
-    Check if current hovered text is a section title and then return it's description
+    Check if current hovered text is a section title and then return it's description.
 
     ARGUMENTS:
     line_str (str): the current line as a string.
@@ -219,20 +227,61 @@ def section_resolver(
 
     # break section into a list of components
     sections_list = line_str[1:-2].split(".")
+    main_section = sections_list[0]
+    sub_section = sections_list[1] if len(sections_list) > 1 else ""
+
     # if the current hover word is in the dictionary of descriptions
     if current_word in CONFIG_DESCRIPTIONS.keys():
-        # TODO Fine-Tune display message
-        hover_display = f"## ⚙️ {current_word}\n{CONFIG_DESCRIPTIONS[current_word]}"
-        return HoverInfo(hover_display, w_start, w_end)
-    elif current_word == sections_list[1] and sections_list[0] in config_schemas.keys():
+        hover_display = (
+            f"(*section*) **{current_word}**: {CONFIG_DESCRIPTIONS[current_word]}"
+        )
+        return SpanInfo(hover_display, w_start, w_end)
+    elif current_word == sub_section and main_section in config_schemas.keys():
         # get field title from the config schema
         field_title = (
-            config_schemas[sections_list[0]]
-            .__fields__[sections_list[1]]
-            .field_info.title
+            config_schemas[main_section].__fields__[sub_section].field_info.title
         )
-        # TODO Fine-Tune display message
-        hover_display = f"## ⚙️ {sections_list[0]} -> {sections_list[1]}\n{field_title}"
-        return HoverInfo(hover_display, w_start, w_end)
+        hover_display = (
+            f"(*section*) {main_section} -> **{sub_section}**: {field_title}"
+        )
+        return SpanInfo(hover_display, w_start, w_end)
     else:
         return None
+
+
+def variable_resolver(
+    line_str: str,
+    config_dict: dict,
+) -> Optional[SpanInfo]:
+    """
+    Check if current hovered text is a variable and then return it's value.
+
+    ARGUMENTS:
+    line_str (str): the current line as a string.
+    config_dict (dict): the config file as a dictionary
+
+    EXAMPLES:
+    ${system.seed}
+    ${components.tok2vec.model.encode.width}
+    """
+
+    # match the variables, always enclosed with ${<string>}
+    variable_regex = r"(?<=\$\{)[^][]*(?=})"
+    variable_match = re.search(variable_regex, line_str)
+
+    if variable_match is None:
+        return None
+
+    variable_list = variable_match.group(0).split(".")
+    v_start = variable_match.span()[0]
+    v_end = variable_match.span()[1] - 1
+
+    # get value for final item in the variable from nested config dict
+    for key in variable_list[:-1]:
+        config_dict = config_dict.setdefault(key, {})
+    variable_value = config_dict[variable_list[-1]]
+
+    hover_display = (
+        f"(*variable*) **{'.'.join(variable_list)}**: `{str(variable_value)}`"
+    )
+    return SpanInfo(hover_display, v_start, v_end)
