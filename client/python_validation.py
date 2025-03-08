e79059ec2ccb8e1468ca2526d3679dc5df128d2b
Igor Morgado <morgado.igor@gmail.com>
2025-03-08 15:45:41 -0300

    Handle imports in python<3.9

--- client/python_validation.py
+++ client/python_validation.py
@@ -1,7 +1,11 @@
 import sys
 from distutils.version import LooseVersion
-import importlib.metadata
 import argparse
+
+if sys.version_info < (3, 9):
+    import importlib_metadata as metadata
+else:
+    import importlib.metadata as metadata
 
 parser = argparse.ArgumentParser(description="Script to validate python interpreter")
 parser.add_argument("pygls_version", help="Required version of pygls")
@@ -10,9 +14,9 @@ args = parser.parse_args()
 
 
 try:
-    pygls_version = importlib.metadata.version("pygls")
+    pygls_version = metadata.version("pygls")
     if LooseVersion(pygls.__version__) >= LooseVersion(args.pygls_version):
-        spacy_version = importlib.metadata.version("spacy")
+        spacy_version = metadata.version("spacy")
         try:
             if LooseVersion(spacy_version) >= LooseVersion(args.spacy_version):
                 sys.stdout.write("I003")
