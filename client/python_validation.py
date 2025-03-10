9fa92737b967e85ef0d0c8019c3004c3e249c9f2
Igor Morgado <morgado.igor@gmail.com>
2025-03-10 10:16:56 -0300

    More robust version checks (#33)
    * More robust version checks
    
    * Handle imports in python<3.9
    
    * Fixed typos

--- client/python_validation.py
+++ client/python_validation.py
@@ -2,19 +2,23 @@ import sys
 from distutils.version import LooseVersion
 import argparse
 
+if sys.version_info < (3, 9):
+    import importlib_metadata as metadata
+else:
+    import importlib.metadata as metadata
+
 parser = argparse.ArgumentParser(description="Script to validate python interpreter")
 parser.add_argument("pygls_version", help="Required version of pygls")
 parser.add_argument("spacy_version", help="Required version of spaCy")
 args = parser.parse_args()
 
+
 try:
-    import pygls
-
-    if LooseVersion(pygls.__version__) >= LooseVersion(args.pygls_version):
+    pygls_version = metadata.version("pygls")
+    if LooseVersion(pygls_version) >= LooseVersion(args.pygls_version):
+        spacy_version = metadata.version("spacy")
         try:
-            import spacy
-
-            if LooseVersion(spacy.__version__) >= LooseVersion(args.spacy_version):
+            if LooseVersion(spacy_version) >= LooseVersion(args.spacy_version):
                 sys.stdout.write("I003")
                 sys.exit()
             else:
